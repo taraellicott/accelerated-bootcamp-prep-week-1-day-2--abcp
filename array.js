2cf70fa6f77070a03ce0732a15a9b36cabccab5b
Tara Ellicott <taraellicott@gmail.com>
2018-01-18 21:33:53 +0000

    automatically backed up by learn

--- array.js
+++ array.js
@@ -1,31 +1,46 @@
 function addElementToBeginningOfArray(array, element) {
  // add the element to the beginning of the array without mutating the array
+ return [element, ...array];
 }
+
 
 function destructivelyAddElementToBeginningOfArray(array, element){
   // add element to the beginning of the array by mutating the array
+  array.unshift(element);
+  return array;
 }
+
 
 function addElementToEndOfArray(array, element) {
-
+ return [...array, element];
 }
+
 
 function destructivelyAddElementToEndOfArray(array, element) {
-
+   array.push(element);
+   return array;
 }
 
+
 function accessElementInArray(array, index) {
+  return array[index];
 
 }
 
 function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
 
 }
 
 function removeElementFromBeginningOfArray(array) {
+  
 }
 
-function destructivelyRemoveElementFromEndOfArray(array) {
+function destructivelyRemoveElementFromEndOfArray(array)
+{
+  array.pop();
+  return array;
 
 }
 
